d8da1f5f5cc3a3100b7d109483566baa246d75e6
gwhitaker <garth.whitaker@metropolitan.co.za>
2016-12-07 14:58:56 +0200

    Added projects page

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+        description: 'This is a projects page.'
+
+    });
+});
+
 app.get('/bad', (req, res) => {
 
     res.send({
